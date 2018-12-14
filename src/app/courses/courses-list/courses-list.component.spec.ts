620dab44dc24e5a1823dce198a2100a010f27f32
Ruslan Petrukhin <Ruslan_Petrukhin@epam.com>
2018-12-14 19:18:01 +0300

    Corrected failed test

--- src/app/courses/courses-list/courses-list.component.spec.ts
+++ src/app/courses/courses-list/courses-list.component.spec.ts
@@ -4,35 +4,42 @@ import { By } from '@angular/platform-browser';
 
 import { CoursesListComponent } from './courses-list.component';
 import { CoursesService } from '../courses.service';
+import { CoursesListItem } from 'src/app-entities/classes/courses-list-item.model';
 
 describe('CoursesListComponent', () => {
   let component: CoursesListComponent;
   let fixture: ComponentFixture<CoursesListComponent>;
   let mockCoursesService: Partial<CoursesService>;
+  let mockCourses: Array<CoursesListItem>;
 
   beforeEach(async(() => {
+    mockCourses = [
+      {
+        id: 1,
+        title: 'Video Course 1',
+        creationDate: new Date(2018, 6, 2),
+        duration: 88,
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
+      },
+      {
+        id: 2,
+        title: 'Video Course 2',
+        creationDate: new Date(2018, 11, 28),
+        duration: 27,
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
+      },
+    ];
+
     mockCoursesService = {
       getCourses() {
-        return [
-          {
-            id: 1,
-            title: 'Video Course 1',
-            creationDate: new Date(2018, 6, 2),
-            duration: 88,
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
-          },
-          {
-            id: 2,
-            title: 'Video Course 2',
-            creationDate: new Date(2018, 11, 28),
-            duration: 27,
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
-          },
-        ];
+        return mockCourses;
+      },
+      deleteCourse(id: number) {
+        return;
       },
     };
 
@@ -54,32 +61,55 @@ describe('CoursesListComponent', () => {
   });
 
   it('should get courses on init', () => {
-    expect(component.courses).toEqual(mockCoursesService.getCourses());
+    expect(component.courses).toEqual(mockCourses);
   });
 
   it('should have correct number of li elements', () => {
     const li = fixture.nativeElement.querySelectorAll('li');
 
-    expect(li.length).toBe(mockCoursesService.getCourses().length);
+    expect(li.length).toBe(mockCourses.length);
   });
 
-  // This describe doesn't work
   describe('on click delete', () => {
-    let button: DebugElement;
+    let spyGetCourses: jasmine.Spy;
+    let spyDeleteCourse: jasmine.Spy;
+    let dbg: DebugElement;
 
     beforeEach(() => {
-      button = fixture.debugElement.query(By.css('app-courses-list-item'));
-      button.triggerEventHandler('deleted', 1);
+      const courseService = TestBed.get(CoursesService);
+
+      spyGetCourses = spyOn(courseService, 'getCourses').and.returnValue([
+        {
+          id: 2,
+          title: 'Video Course 2',
+          creationDate: new Date(2018, 11, 28),
+          duration: 27,
+          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
+        },
+      ]);
+      spyDeleteCourse = spyOn(courseService, 'deleteCourse');
+
+      dbg = fixture.debugElement.query(By.css('app-courses-list-item'));
+      dbg.triggerEventHandler('deleted', 1);
+
+      fixture.detectChanges();
+    });
+
+    it('should call delete and get methods of coursesService', () => {
+      expect(spyDeleteCourse).toHaveBeenCalledWith(1);
+      expect(spyGetCourses).toHaveBeenCalled();
     });
 
     it('should update courses variable', () => {
-      expect(component.courses).toEqual([mockCoursesService.getCourses()[1]]);
+      expect(component.courses).toEqual([mockCourses[1]]);
     });
 
     it('should update number of li elements', () => {
       const li = fixture.nativeElement.querySelectorAll('li');
 
-      expect(li.length).toBe(mockCoursesService.getCourses().length - 1);
+      expect(li.length).toBe(mockCourses.length - 1);
     });
   });
 
